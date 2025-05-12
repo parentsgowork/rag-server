b72d893020a107053a600c1ce453ca8b33caf1bc
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2025-05-12 12:44:41 +0900

    [FEAT] CORS 설정

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # fast api 앱 실행 파일
 from fastapi import FastAPI
 from app.api import routes
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.db import engine
 from app.db_models.education import EducationInfo
 from app.db_models.user import User  # 새로 만들 예정
@@ -8,6 +9,14 @@ from app.core.db import Base
 
 
 app = FastAPI(title="RAG Chatbot API", description="RAG 챗봇 API", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://parentsgowork.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 라우터 등록
 app.include_router(routes.router)
